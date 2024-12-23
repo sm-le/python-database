960ab4085d6bdf2602cee9c3986c80762cf71713
sm-le <sungmin.lee.bio@gmail.com>
2024-12-22 22:50:41 -0800

    add: yaml | change: version change

--- setup.py
+++ setup.py
@@ -1,14 +1,14 @@
 from setuptools import setup, find_packages
 
 setup(  name='database-connector',
-        version='1.0.1',
+        version='1.0.2',
         author='Sungmin Lee',
-        author_email='sungmin.lee.bio@gmail.com',
+        author_email='il.sungminlee@gmail.com',
         description="Python-based database connector for MySQL, MongoDB, Azure Table, and plus",
-        license="MIT License",
+        license="BSD License",
         url="https://github.com/sm-le/python-database-connector",
         package_dir = {'':'src'},
         packages=find_packages(where='src'),
         install_requires=["pymysql","pymongo","DBUtils","azure-data-tables",
                           "azure-identity","azure-keyvault-secrets","python-dotenv",
-                          "zstandard","aiohttp"] )
+                          "zstandard","aiohttp","PyYAML"] )
